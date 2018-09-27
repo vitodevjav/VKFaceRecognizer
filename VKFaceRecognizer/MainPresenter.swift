6c48a5f7fc0fe6b8397b8275316dd16784668b6b
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2018-09-27 16:54:49 +0300

    Added presenter

---
+++ VKFaceRecognizer/MainPresenter.swift
@@ -0,0 +1,28 @@
+//
+//  MainPresenter.swift
+//  VKFaceRecognizer
+//
+//  Created by Kazakevich, Vitaly on 9/27/18.
+//  Copyright © 2018 Kazakevich, Vitaly. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol MainPresenterProtocol {
+    var selectedImage: Variable<UIImage> { get }
+}
+
+class MainPresenter: MainPresenterProtocol {
+    var selectedImage: Variable<UIImage>
+
+    init() {
+        guard let placeholder = UIImage(named: "imagePlaceholder") else {
+            fatalError("Add placeholder first!")
+        }
+
+        selectedImage = Variable(placeholder)
+    }
+
+}
