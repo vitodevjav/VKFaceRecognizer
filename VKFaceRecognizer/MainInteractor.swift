e9ff4851a227de79525e37b3a1a222dc19cdc4cb
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2018-09-27 16:30:09 +0300

    Added Interactor

---
+++ VKFaceRecognizer/MainInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  MainInteractor.swift
+//  VKFaceRecognizer
+//
+//  Created by Kazakevich, Vitaly on 9/27/18.
+//  Copyright © 2018 Kazakevich, Vitaly. All rights reserved.
+//
+
+import Foundation
